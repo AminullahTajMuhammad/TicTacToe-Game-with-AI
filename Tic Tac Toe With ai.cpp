1b79ca42fccaf4f124f5e66eaa4026433ec26fc9
Aminullh Taj Muhammad <aminullahtajmuhammad@gmail.com>
2019-01-28 23:39:11 +0500

    Added Ai function and working on it

--- Tic Tac Toe With ai.cpp
+++ Tic Tac Toe With ai.cpp
@@ -1,169 +1,179 @@
+// ToDo Before this game
+// Implement remove function in tree node
+
 #include<iostream>
 #include<conio.h>
 #include<cstdlib>
 #include<cstring>
 #include<iomanip>				// for setw() function
 #include<Windows.h>				// for coloring and x,y coordinates for consol
+#define MAX_MOVES 9
 using namespace std;
 HANDLE hConsole;
+
+// Global Variables
 bool isGame = true;
+int movesScore[MAX_MOVES];
+//---------------//
+
 //---------- X,Y Co-Ordinates Function ---------------//
-void gotoxy(int x, int y) { 
-    COORD coord;
-    coord.X = x; 
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+struct GameNode {
+	char nodeBox[3][3];
+	int score;
+	GameNode *node;
+};
 class TicTacToe {
-	private:
-		char box[3][3] = { 
-							{' ', ' ', ' '}, 
-							{' ', ' ', ' '}, 
-							{' ', ' ', ' '}
-						};
-		int positionForUser;
-		string Player1, Player2;
-		
-	public:
-		void printBoard() {
-			cout << endl;
-			gotoxy(10,2);
-			cout << setw(4) << box[0][0] << "\t|" << setw(4) << box[0][1] << "\t|" << setw(4) << box[0][2];
-			gotoxy(6,3);
-			cout << " ----------------------------";
-			gotoxy(10,4);
-			cout << setw(4) << box[1][0] << "\t|" << setw(4) << box[1][1] << "\t|" << setw(4) << box[1][2];
-			gotoxy(6,5);
-			cout << " ----------------------------";
-			gotoxy(10,6);
-			cout << setw(4) << box[2][0] << "\t|" << setw(4) << box[2][1] << "\t|" << setw(4) << box[2][2];
+private:
+	static char box[3][3];
+	int positionForUser;
+	string Player1, Player2;
+
+public:
+	void printBoard() {
+		cout << endl;
+		gotoxy(10, 2);
+		cout << setw(4) << box[0][0] << "\t|" << setw(4) << box[0][1] << "\t|" << setw(4) << box[0][2];
+		gotoxy(6, 3);
+		cout << " ----------------------------";
+		gotoxy(10, 4);
+		cout << setw(4) << box[1][0] << "\t|" << setw(4) << box[1][1] << "\t|" << setw(4) << box[1][2];
+		gotoxy(6, 5);
+		cout << " ----------------------------";
+		gotoxy(10, 6);
+		cout << setw(4) << box[2][0] << "\t|" << setw(4) << box[2][1] << "\t|" << setw(4) << box[2][2];
+	}
+
+	void giveinput() {
+	xyz:
+		hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		gotoxy(1, 8);
+		cout << "X player turn:";
+		gotoxy(1, 9);
+		cout << "Enter Position in numbers: ";
+		cin >> positionForUser;
+		if (positionForUser == 1) {
+			if (box[0][0] == ' ') {
+				box[0][0] = 'X';
+			}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 8);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
+			}
 		}
-		
-		void giveinput() {
-			xyz:
-			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			gotoxy(1,8);
-			cout << "X player turn:";
-			gotoxy(1,9);
-			cout << "Enter Position in numbers: ";
-			cin >> positionForUser;
-			if (positionForUser == 1) {
-				if (box[0][0] == ' ') {
-					box[0][0] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 8);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+		else if (positionForUser == 2) {
+			if (box[0][1] == ' ') {
+				box[0][1] = 'X';
 			}
-			else if (positionForUser == 2) {
-				if (box[0][1] == ' ') {
-					box[0][1] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 7);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 7);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
 			}
-			else if (positionForUser == 3) {
-				if (box[0][2] == ' ') {
-					box[0][2] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 9);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+		}
+		else if (positionForUser == 3) {
+			if (box[0][2] == ' ') {
+				box[0][2] = 'X';
 			}
-			if (positionForUser == 4) {
-				if (box[1][0] == ' ') {
-					box[1][0] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 10);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 9);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
 			}
-			if (positionForUser == 5) {
-				if (box[1][1] == ' ') {
-					box[1][1] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 11);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+		}
+		if (positionForUser == 4) {
+			if (box[1][0] == ' ') {
+				box[1][0] = 'X';
 			}
-			if (positionForUser == 6) {
-				if (box[1][2] == ' ') {
-					box[1][2] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 12);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 10);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
 			}
-			if (positionForUser == 7) {
-				if (box[2][0] == ' ') {
-					box[2][0] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 13);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+		}
+		if (positionForUser == 5) {
+			if (box[1][1] == ' ') {
+				box[1][1] = 'X';
 			}
-			if (positionForUser == 8) {
-				if (box[2][1] == ' ') {
-					box[2][1] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 14);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 11);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
 			}
-			if (positionForUser == 9) {
-				if (box[2][2] == ' ') {
-					box[2][2] = 'X';
-				}
-				else {
-					gotoxy(1, 18);
-					SetConsoleTextAttribute(hConsole, 15);
-					cout << "This position is filled enter empty positionForUser: \n\n";
-					SetConsoleTextAttribute(hConsole, 15);
-					goto xyz;
-				}
+		}
+		if (positionForUser == 6) {
+			if (box[1][2] == ' ') {
+				box[1][2] = 'X';
 			}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 12);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
+			}
+		}
+		if (positionForUser == 7) {
+			if (box[2][0] == ' ') {
+				box[2][0] = 'X';
+			}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 13);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
+			}
+		}
+		if (positionForUser == 8) {
+			if (box[2][1] == ' ') {
+				box[2][1] = 'X';
+			}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 14);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
+			}
+		}
+		if (positionForUser == 9) {
+			if (box[2][2] == ' ') {
+				box[2][2] = 'X';
+			}
+			else {
+				gotoxy(1, 18);
+				SetConsoleTextAttribute(hConsole, 15);
+				cout << "This position is filled enter empty positionForUser: \n\n";
+				SetConsoleTextAttribute(hConsole, 15);
+				goto xyz;
+			}
+		}
 		printBoard();
 	}
-	
+
 	void updateInputIntoZero() {
 	AGAIN:
-		gotoxy(1,12);
+		gotoxy(1, 12);
 		cout << "O player turn: ";
-		gotoxy(1,13);
+		gotoxy(1, 13);
 		cout << "Enter Position in numbers: ";
 		cin >> positionForUser;
 		if (positionForUser == 1) {
@@ -247,7 +257,8 @@ class TicTacToe {
 		else if (positionForUser == 7) {
 			if (box[2][0] == ' ') {
 				SetConsoleTextAttribute(hConsole, 10);
-				box[2][0] = 'O';		}
+				box[2][0] = 'O';
+			}
 			else {
 				gotoxy(1, 18);
 				SetConsoleTextAttribute(hConsole, 13);
@@ -285,72 +296,105 @@ class TicTacToe {
 		SetConsoleTextAttribute(hConsole, 7);
 		printBoard();
 	}
-	
+
 	bool checkWin() {
 		// Check For X wins
-		if( box[0][0] == 'X' && box[0][1] == 'X' && box[0][2] == 'X' || box[1][0] == 'X' && box[1][1] == 'X' && box[1][2] == 'X' || 
+		if (box[0][0] == 'X' && box[0][1] == 'X' && box[0][2] == 'X' || box[1][0] == 'X' && box[1][1] == 'X' && box[1][2] == 'X' ||
 			box[2][0] == 'X' && box[2][1] == 'X' && box[2][2] == 'X' || box[0][0] == 'X' && box[1][0] == 'X' && box[2][0] == 'X' ||
-			box[0][1] == 'X' && box[1][1] == 'X' && box[2][1] == 'X' || box[0][2] == 'X' && box[1][2] == 'X' && box[2][2] == 'X' || 
+			box[0][1] == 'X' && box[1][1] == 'X' && box[2][1] == 'X' || box[0][2] == 'X' && box[1][2] == 'X' && box[2][2] == 'X' ||
 			box[0][0] == 'X' && box[1][1] == 'X' && box[2][2] == 'X' || box[0][2] == 'X' && box[1][1] == 'X' && box[2][0] == 'X') {
-			
+
+
 			gotoxy(1, 20);
-			cout << "X player Wins Means " << Player1 << " Wins: " << endl;
-			return false;
-		
+			cout << "X player Wins: " << endl;
+			return true;
+
 		}
 
 		// Check For O wins
-		if( box[0][0] == 'O' && box[0][1] == 'O' && box[0][2] == 'O' || box[1][0] == 'O' && box[1][1] == 'O' && box[1][2] == 'O' || 
+		if (box[0][0] == 'O' && box[0][1] == 'O' && box[0][2] == 'O' || box[1][0] == 'O' && box[1][1] == 'O' && box[1][2] == 'O' ||
 			box[2][0] == 'O' && box[2][1] == 'O' && box[2][2] == 'O' || box[0][0] == 'O' && box[1][0] == 'O' && box[2][0] == 'O' ||
-			box[0][1] == 'O' && box[1][1] == 'O' && box[2][1] == 'O' || box[0][2] == 'O' && box[1][2] == 'O' && box[2][2] == 'O' || 
+			box[0][1] == 'O' && box[1][1] == 'O' && box[2][1] == 'O' || box[0][2] == 'O' && box[1][2] == 'O' && box[2][2] == 'O' ||
 			box[0][0] == 'O' && box[1][1] == 'O' && box[2][2] == 'O' || box[0][2] == 'O' && box[1][1] == 'O' && box[2][0] == 'O') {
-		
+
 			gotoxy(1, 20);
-			cout << "O player Wins Means " << Player2 << " Wins: " << endl;
-			return false;
-		
+			cout << "O player Wins: " << endl;
+			return true;
+
 		}
-		
-		return true;
+
+		return false;
 	}
-	
-	void isDraw() {
-		if( box[0][0] != ' ' && box[0][1] != ' ' && box[0][2] != ' ' &&
-			box[1][0] != ' ' && box[1][1] != ' ' && box[1][2] != ' ' && 
-			box[2][0] != ' ' && box[2][1] != ' ' && box[2][2] != ' ' && checkWin() == true) {
-			
-				gotoxy(1, 20);
-				cout << "Match is Draw " << endl;
-		
+
+	bool isDraw() {
+		if (box[0][0] != ' ' && box[0][1] != ' ' && box[0][2] != ' ' &&
+			box[1][0] != ' ' && box[1][1] != ' ' && box[1][2] != ' ' &&
+			box[2][0] != ' ' && box[2][1] != ' ' && box[2][2] != ' ' && checkWin() == false) {
+
+			return true;
 		}
+		return false;
 	}
-	
+
+	int possibleMoveForComputer();
+};
+char TicTacToe::box[3][3] = {
+	{ 'O', ' ', 'X' },
+	{ 'X', ' ', ' ' },
+	{ 'X', 'O', 'O' }
 };
 int main() {
 	TicTacToe tictactoe;
-	
-	while(isGame == true) {
+
+	while (isGame == true) {
 		bool isCheckForWinner;
 		bool isDraw;
-		
+
 		tictactoe.printBoard();
 		tictactoe.giveinput();
-		
+
 		// return a value to check that the match who wins
 		// check win for X player
-		isCheckForWinner = tictactoe.checkWin();
-		if(isCheckForWinner == false) {
+		if (tictactoe.checkWin()) {
 			break;
 		}
-		tictactoe.isDraw();
+		if (tictactoe.isDraw()) {
+			gotoxy(1, 20);
+			cout << "Match is Draw " << endl;
+		}
+		int value = tictactoe.possibleMoveForComputer();
 		tictactoe.updateInputIntoZero();
-	
+
 		// return a value to check that the match who wins
 		// check win for O player
-		isCheckForWinner = tictactoe.checkWin();
-		if(isCheckForWinner == false) {
+		if (tictactoe.checkWin()) {
 			break;
 		}
-		tictactoe.isDraw();
+		if (tictactoe.isDraw()) {
+			gotoxy(1, 20);
+			cout << "Match is Draw " << endl;
+		}
 	}
+
+	_getch();
+}
+
+int TicTacToe::possibleMoveForComputer() {
+	if (TicTacToe::checkWin() || TicTacToe::isDraw()) {
+		return 10;
+	}
+	else {
+		GameNode *node = new GameNode;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				node->nodeBox[i][j] = box[i][j];
+			}
+		}
+		for (int i = 0; i < 9; i++) {
+			movesScore[i] = node->score;
+		}
+
+	}
+
+	return 0;
 }
