df342962f46657237f8a17c09936a77cce232437
Aminullh Taj Muhammad <aminullahtajmuhammad@gmail.com>
2019-02-17 15:06:13 +0500

    Added methods in tictactoetree file

--- TicTacToeTree.h
+++ TicTacToeTree.h
@@ -1,14 +1,39 @@
 #include<iostream>
-#include"TreeNode"
 #include<cstdlib>
+#include"TreeNode.h"
 using namespace std;
 class TicTacToeTree {
 public:
 	TreeNode *root;
+	int childCount = 0;
+	
+	TicTacToeTree() {
+		root = NULL;
+		childCount = -1;
+	}
+	
+	void addNodes(char box[][3]) {
+		TreeNode *temp = new TreeNode();
+	}
 
+	void addChildInRoot(TreeNode *child) {
+		TreeNode *temp = new TreeNode();
+		if (root == NULL) {
+			return;
+		}
 
-	TicTacToeTree() : root(NULL) {}
+		child->addChild(root);
+	}
+	
+	void addChild(TreeNode *parent, TreeNode *child) {
 
+	}
 
+	void removeChild() {
 
+	}
+
+	void  removeLeafChild() {
+
+	}
 };
